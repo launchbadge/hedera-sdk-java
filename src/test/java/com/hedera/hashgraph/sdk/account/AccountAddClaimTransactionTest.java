4576c6b0bb370cb649c5d95905de3a06149f79a2
austin <austin@launchbadge.com>
2019-05-28 12:43:10 -0700

    add missing serialization test for `AccountAddClaimTransaction`

--- src/test/java/com/hedera/hashgraph/sdk/account/AccountAddClaimTransactionTest.java
+++ src/test/java/com/hedera/hashgraph/sdk/account/AccountAddClaimTransactionTest.java
@@ -7,8 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import java.time.Instant;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 class AccountAddClaimTransactionTest {
     private static final Ed25519PrivateKey key = Ed25519PrivateKey.fromString("302e020100300506032b6570042204203b054fade7a2b0869c6bd4a63b7017cbae7855d12acc357bea718e2c3e805962");
@@ -17,20 +16,12 @@ class AccountAddClaimTransactionTest {
 
     private static final byte[] hash = {1, 2, 2, 3, 3, 3};
 
-    private static final AccountAddClaimTransaction txn = new AccountAddClaimTransaction()
+    private final AccountAddClaimTransaction txn = new AccountAddClaimTransaction()
         .setNodeAccountId(new AccountId(3))
         .setTransactionId(new TransactionId(new AccountId(1234), Instant.parse("2019-04-08T07:04:00Z")))
         .setAccountId(account)
         .setHash(hash)
         .addKey(key.getPublicKey());
-
-    private static final String txnToString = "sigMap {\n" +
-        "  sigPair {\n" +
-        "    pubKeyPrefix: \"\\344\\361\\300\\353L}\\315\\303\\347\\353\\021p\\263\\b\\212=\\022\\242\\227\\364\\243\\353\\342\\362\\205\\003\\375g5F\\355\\216\"\n" +
-        "    ed25519: \"\\2317\\344\\0278u\\006\\223\\212\\317lj{\\343,\\301\\301\\266\\002!08ty\\355\\303\\344\\302W\\266xa\\'\\316\\0312\\316\\035][\\017\\001;\\363rt\\234\\\"\\262\\022\\336\\b\\343\\240\\200\\257_&\\200\\217f\\006\\305\\006\"\n" +
-        "  }\n" +
-        "}\n" +
-        "bodyBytes: \"\\n\\r\\n\\006\\b\\340\\344\\253\\345\\005\\022\\003\\030\\322\\t\\022\\002\\030\\003\\030\\240\\215\\006\\\"\\002\\bxR5\\n3\\n\\003\\030\\350\\a\\022\\006\\001\\002\\002\\003\\003\\003\\032\\$\\n\\\"\\022 \\344\\361\\300\\353L}\\315\\303\\347\\353\\021p\\263\\b\\212=\\022\\242\\227\\364\\243\\353\\342\\362\\205\\003\\375g5F\\355\\216\"\n";
 
     @Test
     @DisplayName("collect transaction validates")
@@ -52,4 +43,20 @@ class AccountAddClaimTransactionTest {
                 ".addKey() required"
         );
     }
+
+    @Test
+    @DisplayName("transaction serializes correctly")
+    void transactionSerializesCorrectly() {
+        assertEquals(
+            txn.sign(key).toProto().toString(),
+            "sigMap {\n" +
+                "  sigPair {\n" +
+                "    pubKeyPrefix: \"\\344\\361\\300\\353L}\\315\\303\\347\\353\\021p\\263\\b\\212=\\022\\242\\227\\364\\243\\353\\342\\362\\205\\003\\375g5F\\355\\216\"\n" +
+                "    ed25519: \"\\2317\\344\\0278u\\006\\223\\212\\317lj{\\343,\\301\\301\\266\\002!08ty\\355\\303\\344\\302W\\266xa\\'\\316\\0312\\316\\035][\\017\\001;\\363rt\\234\\\"\\262\\022\\336\\b\\343\\240\\200\\257_&\\200\\217f\\006\\305\\006\"\n" +
+                "  }\n" +
+                "}\n" +
+                "bodyBytes: \"\\n\\r\\n\\006\\b\\340\\344\\253\\345\\005\\022\\003\\030\\322\\t\\022\\002\\030\\003\\030\\240\\215\\006\\\"\\002\\bxR5\\n3\\n\\003\\030\\350\\a\\022\\006\\001\\002\\002\\003\\003\\003\\032$\\n\\\"\\022 \\344\\361\\300\\353L}\\315\\303\\347\\353\\021p\\263\\b\\212=\\022\\242\\227\\364\\243\\353\\342\\362\\205\\003\\375g5F\\355\\216\"\n"
+        );
+    }
+
 }
